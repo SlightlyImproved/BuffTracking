08ca77cb4ce499ea27cb8fa6869ab625055f56e7
Arthur Corenzan <arthur@corenzan.com>
2016-04-24 20:30:31 -0300

    Rewrite; Fix #5 and #4

--- SlightlyImprovedBuffTracking.lua
+++ SlightlyImprovedBuffTracking.lua
@@ -1,36 +1,150 @@
 -- Slightly Improved™ Buff Tracking 1.0.1 (Apr 22 2016)
 -- Licensed under CC BY-NC-SA 4.0
 
-NAMESPACE = "SlightlyImprovedBuffTracking"
+local NAMESPACE = "SlightlyImprovedBuffTracking"
 
--- local function d() end
-local FADING_ANIMATION_THRESHOLD = 3
+local function d() end
+local function df() end
 
-function Sibt_Effect_OnMouseEnter(control)
+local PINGPONG_ANIMATION_THRESHOLD = 3
+
+local SORT_VALUE_FOR_DEBUFF = 999000
+local SORT_VALUE_FOR_LONGTERM = 99000
+
+--
+--
+--
+
+local BuffTracker = ZO_Object:Subclass()
+
+function BuffTracker:New(parent)
+    local tracker = ZO_Object.New(self)
+
+    tracker.control = CreateControlFromVirtual("Sibt_BuffTracker1", parent, "Sibt_BuffTracker")
+    tracker.pool = ZO_ControlPool:New("Sibt_BuffTrackerEntry", tracker.control, "Entry")
+
+    tracker.pool:SetCustomFactoryBehavior(function(control)
+        ZO_AlphaAnimation:New(control)
+        control.effectStack = {}
+    end)
+
+    tracker.pool:SetCustomResetBehavior(function(control)
+        control:ClearAnchors()
+        control.effectStack = {}
+    end)
+
+    return tracker
+end
+
+function BuffTracker:Reset()
+    self.pool:ReleaseAllObjects()
+end
+
+function BuffTracker:AddEffect(effect)
+    if (effect.buffSlot > 0 and effect.buffName ~= "") then
+        local key = effect.iconFile..effect.effectType
+        local control = self.pool:AcquireObject(key)
+
+        table.insert(control.effectStack, effect)
+        table.sort(control.effectStack, function(effect1, effect2)
+            return (effect1.duration < effect2.duration)
+        end)
+
+        control.shortestEffect = control.effectStack[1]
+
+        GetControl(control, "Icon"):SetTexture(control.shortestEffect.iconFile)
+
+        if (control.shortestEffect.effectType == BUFF_EFFECT_TYPE_DEBUFF) then
+            GetControl(control, "Highlight"):SetHidden(false)
+            control.sortValue = SORT_VALUE_FOR_DEBUFF
+        else
+            if (control.shortestEffect.duration == 0) then
+                control.sortValue = SORT_VALUE_FOR_LONGTERM
+            else
+                control.sortValue = control.shortestEffect.duration
+            end
+        end
+    end
+end
+
+function BuffTracker:Update()
+    local children = {}
+    for _, control in pairs(self.pool:GetActiveObjects()) do
+        table.insert(children, control)
+    end
+
+    table.sort(children, function(child1, child2)
+        if (child1 and child2) then
+            return child1.sortValue > child2.sortValue
+        else
+            return false
+        end
+    end)
+
+    local previousControl
+    for i = 1, #children do
+        local control = children[i]
+        if previousControl then
+            control:SetAnchor(LEFT, previousControl, RIGHT, 2, 0)
+        else
+            control:SetAnchor(LEFT)
+        end
+        previousControl = control
+    end
+end
+
+--
+--
+--
+
+local function StartPingPongAnimation(control)
+    if (not control.isAnimating) then
+        df("STARTING animation for %s", control.shortestEffect.buffName)
+        local animation = ZO_AlphaAnimation_GetAnimation(control)
+        animation:PingPong(1, 0, 600)
+        control.isAnimating = true
+    end
+end
+
+local function StopAnimation(control)
+    if control.isAnimating then
+        df("STOPPING animation for %s", control.shortestEffect.buffName)
+        local animation = ZO_AlphaAnimation_GetAnimation(control)
+        animation:FadeIn(0, 300)
+        control.isAnimating = false
+    end
+end
+
+function Sibt_BuffTrackerEntry_OnMouseEnter(control)
     InitializeTooltip(GameTooltip, control, BOTTOM, 0, -5)
-    for _, effect in ipairs(control.effects) do
+    for _, effect in ipairs(control.effectStack) do
         GameTooltip:AddLine(effect.buffName)
     end
 end
 
-function Sibt_Effect_OnMouseExit(control)
+function Sibt_BuffTrackerEntry_OnMouseExit(control)
     ClearTooltip(GameTooltip)
 end
 
-function Sibt_Effect_OnUpdate(control)
-    local effect = control.effects[1]
-    if effect.isTimed then
-        if (effect.endTime >= GetFrameTimeSeconds()) then
-            local time = effect.endTime - GetFrameTimeSeconds()
+function Sibt_BuffTrackerEntry_OnUpdate(control)
+    if (control.shortestEffect.duration > 0) then
+        if (control.shortestEffect.endTime >= GetFrameTimeSeconds()) then
+            local timeToExpire = control.shortestEffect.endTime - GetFrameTimeSeconds()
 
-            local text = ZO_FormatTime(time, TIME_FORMAT_STYLE_COLONS, TIME_FORMAT_PRECISION_TWELVE_HOUR)
-            GetControl(control, "Time"):SetText(zo_strsplit(" ", text))
+            local text = ""
+            if (timeToExpire > 3600) then
+                text = string.format("%dh", zo_round(timeToExpire / 3600))
+            elseif (timeToExpire > 60) then
+                text = string.format("%dm", zo_round(timeToExpire / 60))
+            else
+                text = string.format("%ds", zo_round(timeToExpire))
+            end
+            GetControl(control, "Time"):SetText(text)
 
-            local animation = ZO_AlphaAnimation_GetAnimation(control)
-            if (time < FADING_ANIMATION_THRESHOLD) then
-                if not animation:IsPlaying() then
-                    animation:PingPong(1, 0, 600)
-                end
+            if (timeToExpire < PINGPONG_ANIMATION_THRESHOLD) then
+                StartPingPongAnimation(control)
+            else
+                StopAnimation(control)
             end
         end
     end
@@ -38,25 +152,11 @@ end
 
 EVENT_MANAGER:RegisterForEvent(NAMESPACE, EVENT_ADD_ON_LOADED, function(eventCode, addOnName)
     if (addOnName == NAMESPACE) then
-        local effectsBar = CreateControlFromVirtual("Sibt_EffectsBar1", ACTION_BAR_FRAGMENT.control, "Sibt_EffectsBar")
-        local effectsPool = ZO_ControlPool:New("Sibt_Effect", effectsBar, "Effect")
+        local tracker = BuffTracker:New(ACTION_BAR_FRAGMENT.control)
 
-        effectsPool:SetCustomFactoryBehavior(function(control)
-            control.effects = {}
-            ZO_AlphaAnimation:New(control)
-        end)
+        local function UpdateBuffTracker()
+            tracker:Reset()
 
-        effectsPool:SetCustomResetBehavior(function(control)
-            control:ClearAnchors()
-            control.effects = {}
-            local animation = ZO_AlphaAnimation_GetAnimation(control)
-            animation:Stop()
-        end)
-
-        local function UpdateEffectsBar()
-            effectsPool:ReleaseAllObjects()
-
-            local previousEffectControl
             for i = 1, GetNumBuffs("player") do
                 local effect = {}
 
@@ -70,41 +170,19 @@ EVENT_MANAGER:RegisterForEvent(NAMESPACE, EVENT_ADD_ON_LOADED, function(eventCod
                 effect.effectType,
                 effect.abilityType,
                 effect.statusEffectType = GetUnitBuffInfo("player", i)
-                effect.isTimed = effect.endTime > effect.startTime
                 effect.duration = effect.endTime - effect.startTime
 
                 -- d(effect, "---")
 
-                if (effect.buffSlot > 0 and effect.buffName ~= "") then
-                    local key = effect.iconFile..effect.effectType
-                    local effectControl = effectsPool:AcquireObject(key)
-                    table.insert(effectControl.effects, effect)
-
-                    table.sort(effectControl.effects, function(a, b)
-                        return a.duration < b.duration
-                    end)
-
-                    GetControl(effectControl, "Icon"):SetTexture(effect.iconFile)
-
-                    if (effect.effectType ~= BUFF_EFFECT_TYPE_BUFF) then
-                        GetControl(effectControl, "Highlight"):SetHidden(false)
-                    end
-                end
+                tracker:AddEffect(effect)
             end
 
-            local previousEffectControl
-            for _, effectControl in pairs(effectsPool:GetActiveObjects()) do
-                if previousEffectControl then
-                    effectControl:SetAnchor(LEFT, previousEffectControl, RIGHT, 2, 0)
-                else
-                    effectControl:SetAnchor(LEFT)
-                end
-                previousEffectControl = effectControl
-            end
+            tracker:Update()
         end
-        EVENT_MANAGER:RegisterForEvent(NAMESPACE, EVENT_EFFECT_CHANGED, UpdateEffectsBar)
-        EVENT_MANAGER:RegisterForEvent(NAMESPACE, EVENT_EFFECTS_FULL_UPDATE, UpdateEffectsBar)
-        EVENT_MANAGER:RegisterForEvent(NAMESPACE, EVENT_PLAYER_ACTIVATED, UpdateEffectsBar)
+
+        EVENT_MANAGER:RegisterForEvent(NAMESPACE, EVENT_EFFECT_CHANGED, UpdateBuffTracker)
+        EVENT_MANAGER:RegisterForEvent(NAMESPACE, EVENT_PLAYER_ACTIVATED, UpdateBuffTracker)
+        EVENT_MANAGER:RegisterForEvent(NAMESPACE, EVENT_EFFECTS_FULL_UPDATE, UpdateBuffTracker)
 
         do
             -- The ActionBar shown at the bottom of the screen and the one shown in the
@@ -114,14 +192,14 @@ EVENT_MANAGER:RegisterForEvent(NAMESPACE, EVENT_ADD_ON_LOADED, function(eventCod
             local show = SKILLS_ACTION_BAR_FRAGMENT.Show
             function SKILLS_ACTION_BAR_FRAGMENT:Show()
                 show(self)
-                effectsBar:SetHidden(true)
+                tracker.control:SetHidden(true)
             end
 
             local onStateChange = SKILLS_ACTION_BAR_FRAGMENT.OnStateChange
             function SKILLS_ACTION_BAR_FRAGMENT:OnStateChange(oldState, newState)
                 onStateChange(self, oldState, newState)
                 if (newState == SCENE_FRAGMENT_HIDDEN) then
-                    effectsBar:SetHidden(false)
+                    tracker.control:SetHidden(false)
                 end
             end
         end
